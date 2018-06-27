4b0b14a11a900c3628261d6573d5477f97c67434
zhaojinliang <zhaojl131415@163.com>
2018-06-27 18:12:53 +0800

    init commit

--- src/main/java/com/zhao/SpringBootAutoloadCacheDemoApplication.java
+++ src/main/java/com/zhao/SpringBootAutoloadCacheDemoApplication.java
@@ -1,4 +1,4 @@
-package com.zhao.demo;
+package com.zhao;
 
 import com.zhao.demo.entity.UserBean;
 import com.zhao.demo.mapper.UserMapper;
@@ -10,7 +10,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement(proxyTargetClass = true)
@@ -20,24 +19,5 @@ public class SpringBootAutoloadCacheDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAutoloadCacheDemoApplication.class, args);
-	}
-
-
-	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-		ApplicationListener<ApplicationReadyEvent> readyListener=new ApplicationListener<ApplicationReadyEvent> () {
-
-			@Override
-			public void onApplicationEvent(ApplicationReadyEvent event) {
-				ConfigurableApplicationContext context = event.getApplicationContext();
-				UserMapper userMapper=context.getBean(UserMapper.class);
-				userMapper.allUsers();
-
-				UserBean user = new UserBean();
-				user.setStatus(1);
-				userMapper.listByCondition(user);
-			}
-
-		};
-		return builder.sources(SpringBootAutoloadCacheDemoApplication.class).bannerMode(Banner.Mode.OFF).listeners(readyListener);
 	}
 }
--- src/main/java/com/zhao/demo/config/AutoloadCacheConfiguration.java
+++ src/main/java/com/zhao/demo/config/AutoloadCacheConfiguration.java
@@ -1,9 +1,11 @@
-package com.zhao.core.config;
+package com.zhao.demo.config;
 
 import com.jarvis.cache.ICacheManager;
+import com.jarvis.cache.autoconfigure.AutoloadCacheProperties;
 import com.jarvis.cache.clone.ICloner;
 import com.jarvis.cache.map.MapCacheManager;
-import com.zhao.core.autoconfigure.AutoloadCacheProperties;
+import com.jarvis.cache.serializer.ISerializer;
+import com.jarvis.cache.serializer.JacksonJsonSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,4 +21,11 @@ public class AutoloadCacheConfiguration {
     public ICacheManager mapCacheManager(AutoloadCacheProperties config, ICloner cloner) {
         return new MapCacheManager(config.getConfig(), cloner);
     }
+
+
+    @Bean
+    public ISerializer<Object> autoloadCacheSerializer() {
+            return new JacksonJsonSerializer();
+        }
+
 }
